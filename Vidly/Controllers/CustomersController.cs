91be55a91ac51cfe600b1cc99e6ba024e40bd85e
Brendon Fatland <b.fatland@gmail.com>
2017-03-17 09:19:01 -0500

    Edit the controller to display the View. Edit the view to idplays the data dynamically and create a class dynamically with Razor.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        [Route("customers/index")]
+        public ActionResult Index()
+        {
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "First one" }
+            };
+
+            var viewModel = new CustomerViewModel
+            {
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+        
+        
+       
+    }
+}
